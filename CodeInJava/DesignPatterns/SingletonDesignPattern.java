ba71cb50ffee229412903c57b8b6e0d5699f1307
akshayswami17 <akshayswami1731@gmail.com>
2024-03-27 09:56:46 +0530

    uploded Singleton pattern program

---
+++ CodeInJava/DesignPatterns/SingletonDesignPattern.java
@@ -0,0 +1,30 @@
+package DesignPatterns;
+
+import java.io.*;
+
+class Singleton {
+    // static class
+    private static Singleton instance;
+
+    private Singleton() {
+        System.out.println("Singleton is Instantiated.");
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null)
+            instance = new Singleton();
+        return instance;
+    }
+
+    public static void doSomething() {
+        System.out.println("Somethong is Done.");
+    }
+}
+
+public class SingletonDesignPattern {
+    public static void main(String[] args) {
+        Singleton.getInstance().doSomething();
+
+    }
+
+}
