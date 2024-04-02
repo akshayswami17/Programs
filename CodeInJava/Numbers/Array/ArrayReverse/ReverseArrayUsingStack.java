45d01e532904818f893c6d0b6081e2ba208407db
akshayswami17 <akshayswami1731@gmail.com>
2024-04-02 17:53:28 +0530

    Add new programs.

---
+++ CodeInJava/Numbers/Array/ArrayReverse/ReverseArrayUsingStack.java
@@ -0,0 +1,26 @@
+package Numbers.Array.ArrayReverse;
+
+import java.util.Stack;
+
+public class ReverseArrayUsingStack 
+{
+    public static void arrayReverse(int arr[])
+    {
+        Stack<Integer> stack = new Stack<>();
+        for(int element : arr){ stack.push(element);}
+        for(int i =0 ; i<arr.length;i++)
+        {
+            arr[i] = stack.pop();
+        }        
+    }
+
+    public static void main(String[] args) {
+        int[] originalArr = {1, 2, 3, 4, 5,6};
+        
+        for(int a : originalArr){System.out.print(a+",");}
+        System.out.println();
+        arrayReverse(originalArr);
+        for(int a : originalArr){System.out.print(a+",");}
+    }
+    
+}
---
+++ CodeInJava/Numbers/Array/CountOccurrences/countOccurrencess.java
@@ -0,0 +1,26 @@
+package Numbers.Array.CountOccurrences;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class countOccurrencess 
+{
+    private static int countOccurrences(ArrayList<Integer> clist , int x)
+    {
+        return Collections.frequency(clist,x);
+        
+    }
+    public static void main(String[] args) 
+    {
+        int arr[] = { 1, 2, 2, 2, 2, 3, 4, 7, 8, 8 };
+        int x = 2;
+        ArrayList<Integer> clist = new ArrayList<>();
+        for(int i : arr){clist.add(i);}
+        System.out.println(x + " occurs "
+                           + countOccurrences(clist, x)
+                           + " times");
+    }
+
+    
+    
+}
