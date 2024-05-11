efd049199eac7ea9c61415e653f7e4e04cf9d190
akshayswami17 <akshayswami1731@gmail.com>
2024-05-11 15:21:45 +0530

    Added to string programms.

---
+++ CodeInJava/Strings/Strings/_7_Findtheminimumdistancebetweenthegiventwowords.java
@@ -0,0 +1,36 @@
+public class _7_Findtheminimumdistancebetweenthegiventwowords 
+{
+    public static int sortestDistance(String[] s , String word1 , String word2)
+    {
+        int d1 = -1 , d2=-1;
+        int ans = Integer.MAX_VALUE;
+        for(int i=0 ; i<s.length ;i++)
+        {
+            if(s[i] == word1)
+            {
+                d1=i;
+            }
+            if(s[i] == word2)
+            {
+                d2=i;
+            }
+            if(d1 != -1 && d2 != -1)
+            {
+                ans = Math.min(ans ,Math.abs(d1-d2));
+            }
+        }
+        return ans;
+        
+    }
+
+    public static void main (String[] args) {
+        String[] S
+        = { "the", "quick", "brown", "fox", "quick" };
+ 
+    String word1 = "the", word2 = "fox";
+ 
+    // Function Call
+    System.out.println(sortestDistance(S, word1, word2));
+    }
+    
+}
